9cb17ab29d1314012e5022c43dbb1c1f98bbea91
uttam anand <ryansuxena@gmail.com>
2025-01-06 21:20:09 -0600

    Paremeterizing the build

--- ChangeExecutionMode_VS.ps1
+++ ChangeExecutionMode_VS.ps1
@@ -1,16 +1,20 @@
+# Get the parameters from the Jenkins environment
+$VirtualServiceName = $env:VirtualServiceName  # Get the VirtualServiceName from Jenkins
+$ExecutionMode = $env:ExecutionMode  # Get the ExecutionMode from Jenkins
+
 # Set the URL for the API endpoint
-$url = "http://localhost:1505/api/Dcm/VSEs/VSE/Broadcom-Cars-Demo-001"
+$url = "http://localhost:1505/api/Dcm/VSEs/VSE/$VirtualServiceName"
 
 # Define the credentials for basic authentication
 $credentials = [System.Management.Automation.PSCredential]::new("admin", ("admin" | ConvertTo-SecureString -AsPlainText -Force))
 
-# Set the XML data to send in the request body
+# Set the XML data to send in the request body, parameterized with $VirtualServiceName and $ExecutionMode
 $xmlBody = @"
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <VirtualService 
-    xmlns="http://www.ca.com/lisa/invoke/v2.0" name="magical" 
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.ca.com/lisa/invoke/v2.0 VirtualService.xsd" href="http://localhost:1505/api/Dcm/VSEs/VSE/magical" type="application/vnd.ca.lisaInvoke.virtualService+xml">
-    <ExecutionMode>Most Efficient</ExecutionMode>
+    xmlns="http://www.ca.com/lisa/invoke/v2.0" name="$VirtualServiceName" 
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.ca.com/lisa/invoke/v2.0 VirtualService.xsd" href="http://localhost:1505/api/Dcm/VSEs/VSE/$VirtualServiceName" type="application/vnd.ca.lisaInvoke.virtualService+xml">
+    <ExecutionMode>$ExecutionMode</ExecutionMode>
     <LearnSuccessResponseOnly>false</LearnSuccessResponseOnly>
 </VirtualService>
 "@
